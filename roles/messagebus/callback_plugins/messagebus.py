ad591132a24f6f6972e99c0199c097076a7ddb5f
emqiu <emqiu@redhat.com>
2016-09-14 16:27:20 -0400

    change back to 2nd attempt

--- roles/messagebus/callback_plugins/messagebus.py
+++ roles/messagebus/callback_plugins/messagebus.py
@@ -29,7 +29,7 @@ import sys
 import tempfile
 import time
 
-#import jenkins
+import jenkins
 
 try:
     import cStringIO as StringIO
@@ -187,5 +187,5 @@ class CallbackModule(CallbackBase):
                 self.messagebus_crt, self.messagebus_key,
                 status_message)).run()
         else:
-            #j = jenkins.Jenkins(self.jenkins_url_addr, self.jenkins_usrname, self.jenkins_api_token)
-            #j.build_job(self.jenkins_jobName, parameters=status_message)
+            j = jenkins.Jenkins(self.jenkins_url_addr, self.jenkins_usrname, self.jenkins_api_token)
+            j.build_job(self.jenkins_jobName, parameters=status_message)
