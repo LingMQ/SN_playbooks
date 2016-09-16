1caf5fa8d3a34ca2357e5f6ef93596d0e9a6b802
emqiu <emqiu@redhat.com>
2016-09-16 10:05:34 -0400

    change jenkins module to requests

--- roles/messagebus/callback_plugins/messagebus.py
+++ roles/messagebus/callback_plugins/messagebus.py
@@ -29,7 +29,8 @@ import sys
 import tempfile
 import time
 
-import jenkins
+#import jenkins
+import requests
 
 try:
     import cStringIO as StringIO
@@ -187,5 +188,13 @@ class CallbackModule(CallbackBase):
                 self.messagebus_crt, self.messagebus_key,
                 status_message)).run()
         else:
-            j = jenkins.Jenkins(self.jenkins_url_addr, self.jenkins_usrname, self.jenkins_api_token)
-            j.build_job(self.jenkins_jobName, parameters=status_message)
+            #j = jenkins.Jenkins(self.jenkins_url_addr, self.jenkins_usrname, self.jenkins_api_token)
+            #j.build_job(self.jenkins_jobName, parameters=status_message)
+            ## use request instead
+            url = self.jenkins_url_addr
+            #req = urllib2.Request(url)
+            resp = requests.post(url,
+                                 params=status_message,
+                                 auth=(self.jenkins_usrname, self.jenkins_api_token),
+                                 verify=False)
+            
